c1f2228d9e282d6dc9ae36a1c7d58ecd9229dba4
creativemoon <157036607+Creativetools90@users.noreply.github.com>
2024-05-18 13:02:42 +0530

    choose media

--- client/src/admin/AdminPost/CreatePost.jsx
+++ client/src/admin/AdminPost/CreatePost.jsx
@@ -5,12 +5,16 @@ const CreatePost = () => {
   // const [content, setContent] = useState(''); // State to hold the content of the editor
   const editorRef = useRef(null);
   const [activeMedia, setActiveMedia] = useState(false); // State to hold the
+  const [chooseMedia, setChooseMedia] = useState("uploeadMedia"); // State to hold the
   const handleInput = (e) => {
     const content = editorRef.current.innerHTML;
     console.log(content);
   };
   const doActiveMedia = () => {
     setActiveMedia(!activeMedia);
+  };
+  const doChooseMedia = (option) => {
+    setChooseMedia(option);
   };
   return (
     <>
@@ -19,16 +23,67 @@ const CreatePost = () => {
           activeMedia ? "uplodeMediaContainer" : "uplodeMediaContainerHide"
         }
       >
+        {/* uplode meddia */}
         <div className="uplodeMedia">
           <div className="chooseMedia">
-            <input type="file" />
+            <div className="fileChoose">
+              <h3>insert media</h3>
+              <div className="mediaOption">
+                <button onClick={() => doChooseMedia("uploeadMedia")}>
+                  uploead media
+                </button>
+                <button onClick={() => doChooseMedia("MediaLib")}>
+                  media libreay
+                </button>
+              </div>
+            </div>
+            {chooseMedia === "uploeadMedia" && (
+             
+             <>
+
+              <div className="uploeadMedua">
+              <label htmlFor="chooseUploadImg">
+              <img width="50" height="50" src="https://img.icons8.com/ios/50/plus--v1.png" alt="plus--v1"/>
+              </label>
+              </div>
+              <input type="file" id="chooseUploadImg" />
+             </>
+            )}
+            {chooseMedia === "MediaLib" && (
+              <div className="gallery">
+                <img src="https://picsum.photos/200/200" alt="" />
+                <img src="https://picsum.photos/200/200" alt="" />
+                <img src="https://picsum.photos/200/200" alt="" />
+                <img src="https://picsum.photos/200/200" alt="" />
+                <img src="https://picsum.photos/200/200" alt="" />
+                <img src="https://picsum.photos/200/200" alt="" />
+                <img src="https://picsum.photos/200/200" alt="" />
+                <img src="https://picsum.photos/200/200" alt="" />
+                <img src="https://picsum.photos/200/200" alt="" />
+                <img src="https://picsum.photos/200/200" alt="" />
+                <img src="https://picsum.photos/200/200" alt="" />
+                <img src="https://picsum.photos/200/200" alt="" />
+                <img src="https://picsum.photos/200/200" alt="" />
+                <img src="https://picsum.photos/200/200" alt="" />
+                <img src="https://picsum.photos/200/200" alt="" />
+                <img src="https://picsum.photos/200/200" alt="" />
+                <img src="https://picsum.photos/200/200" alt="" />
+                <img src="https://picsum.photos/200/200" alt="" />
+                <img src="https://picsum.photos/200/200" alt="" />
+                <img src="https://picsum.photos/200/200" alt="" />
+                <img src="https://picsum.photos/200/200" alt="" />
+                <img src="https://picsum.photos/200/200" alt="" />
+              </div>
+            )}
           </div>
           <div className="previewMedia">
-            <button onClick={doActiveMedia} >close</button>
+            <button onClick={doActiveMedia}>close</button>
             <img src="https://picsum.photos/200/200" alt="" />
           </div>
         </div>
       </div>
+
+      {/*  */}
       <section className="colSec">
         <div className="create_post_container">
           <div className="add">
