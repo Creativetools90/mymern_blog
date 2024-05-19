470a4b5981b85795d5dbbbd0485d5cf0cf77d8ef
creativemoon <157036607+Creativetools90@users.noreply.github.com>
2024-05-19 18:18:10 +0530

    post

--- client/src/admin/CreatePost.jsx
+++ client/src/admin/CreatePost.jsx
@@ -1,11 +1,28 @@
 import React from "react";
-
+import { useState } from "react";
 const CreatePost = () => {
+  const [editmodeZoom, setEditmodeZoom] = useState(false);
+  const [prewmodeZoom, setPrevmodeZoom] = useState(false);
+  const editModeZoomhanddle = () => {
+    setEditmodeZoom(!editmodeZoom);
+  };
+  const pewvModeZoomhanddle = () => {
+    setPrevmodeZoom(!prewmodeZoom);
+  };
   return (
     <>
       <div className="createPostContainer">
-        <div className="editMode">
-          <div className="head">new post</div>
+        <div className={editmodeZoom ? "editModeZoom" : "editMode"}>
+          <div className="head">
+            new post
+            <img
+              width="25"
+              height="25"
+              onClick={editModeZoomhanddle}
+              src="https://img.icons8.com/ios/50/expand--v1.png"
+              alt="expand--v1"
+            />
+          </div>
           <div className="userprofile">
             <div className="pic">
               <div className="profile_pic1">
@@ -18,7 +35,7 @@ const CreatePost = () => {
             </div>
           </div>
           <div className="addTag  titleBar">
-          <input type="text" name="tag" placeholder="Enter your blog ttile" />
+            <input type="text" name="tag" placeholder="Enter your blog ttile" />
           </div>
           <div className="caption">
             <p className="about_pera">caption</p>
@@ -53,14 +70,26 @@ const CreatePost = () => {
                 </label>
                 <input type="file" name="image" id="image" />
               </div>
+              <div className="add">
+                <img src="https://picsum.photos/200/300" alt="" />
+              </div>
             </div>
           </div>
           <div className="timeprosses">
             <button>schedule</button>
           </div>
         </div>
-        <div className="prevideMode">
+        <div className={prewmodeZoom ? "prevideModeZoom" : "prevideMode"}>
           <div className="preview_head">
+            <div className="zoom">
+              <img
+                width="25"
+                height="25"
+                onClick={pewvModeZoomhanddle}
+                src="https://img.icons8.com/ios/50/expand--v1.png"
+                alt="expand--v1"
+              />
+            </div>
             <h3>preview</h3>
             <p className="prePera">
               Preview shows how you content will look when publihsed. Social
