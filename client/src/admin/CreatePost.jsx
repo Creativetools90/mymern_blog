76a637e49be223ec67be75ede41e1d4e962d02f0
creativemoon <157036607+Creativetools90@users.noreply.github.com>
2024-05-19 17:59:43 +0530

    sd

--- client/src/admin/CreatePost.jsx
+++ client/src/admin/CreatePost.jsx
@@ -17,6 +17,9 @@ const CreatePost = () => {
               <button>save post</button>
             </div>
           </div>
+          <div className="addTag  titleBar">
+          <input type="text" name="tag" placeholder="Enter your blog ttile" />
+          </div>
           <div className="caption">
             <p className="about_pera">caption</p>
             <div className="captionEditor" contentEditable></div>
@@ -32,23 +35,37 @@ const CreatePost = () => {
               <button>add</button>
             </div>
             <ul className="tag_list">
-                <li className="mytag">blog</li>
-                <li className="mytag">coding</li>
+              <li className="mytag">blog</li>
+              <li className="mytag">coding</li>
             </ul>
           </div>
           <div className="addImage">
-            
+            <p className="about_pera">add images</p>
+            <div className="imageList">
+              <div className="add">
+                <label htmlFor="image">
+                  <img
+                    width="80"
+                    height="80"
+                    src="https://img.icons8.com/ios/50/add--v1.png"
+                    alt="add--v1"
+                  />
+                </label>
+                <input type="file" name="image" id="image" />
+              </div>
+            </div>
           </div>
           <div className="timeprosses">
-          <button>schedule</button>
+            <button>schedule</button>
           </div>
         </div>
         <div className="prevideMode">
           <div className="preview_head">
             <h3>preview</h3>
             <p className="prePera">
-                Preview shows how you content will look when publihsed.
-                Social network updates may after its final appearence. <a href="">please report then</a>
+              Preview shows how you content will look when publihsed. Social
+              network updates may after its final appearence.{" "}
+              <a href="">please report then</a>
             </p>
           </div>
           <div className="previde_mode_switch"></div>
